b65d96fc7ce810950673ae6ef9d7e1b469bac195
Bucha1958 <okoriemmadu@gmail.com>
2024-01-08 22:12:52 +0100

    added

--- app.js
+++ app.js
@@ -1,14 +1,17 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const cors = require('cors');
 const { connectToDatabase } = require('./src/config/database');
 
 const authRoutes = require('./src/routes/authRoutes');
 const app = express();
 
+app.use(cors());
 // Set up middleware
 app.use(express.json()); 
 // Use authentication routes
 app.use('/auth', authRoutes);
+
 
 // Database connection
 connectToDatabase()
--- src/routes/authRoutes.js
+++ src/routes/authRoutes.js
@@ -1,10 +1,13 @@
 
 const express = require('express');
 const authController = require('../controllers/authController');
+const registerController = require('../controllers/registerController');
 
 const router = express.Router();
 
 // Route for sending a login link
 router.post('/send-login-link', authController.sendLoginLink);
+router.post('/register', registerController.register);
+
 
 module.exports = router;
