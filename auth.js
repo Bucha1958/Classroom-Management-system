cd17f16bd980de6ddc3f74b5cb7d98299c95fb67
Bucha1958 <okoriemmadu@gmail.com>
2024-02-02 10:11:16 +0100

    New callback url added

--- auth.js
+++ auth.js
@@ -8,7 +8,7 @@ const GoogleStrategy = require( 'passport-google-oauth2' ).Strategy;
 passport.use(new GoogleStrategy({
     clientID: process.env.GOOGLE_CLIENT_ID,
     clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-    callbackURL: "http://localhost:3000/auth/google/callback",
+    callbackURL: "https://classroom-system.onrender.com/auth/google/callback",
     passReqToCallback: true
   },
   function(request, accessToken, refreshToken, profile, done) {
