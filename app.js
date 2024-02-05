4cf0789dd3d1142e4e319d37ae2117adf6a8385e
Bucha1958 <okoriemmadu@gmail.com>
2024-02-05 22:56:12 +0100

    resolve cors errors

--- app.js
+++ app.js
@@ -13,7 +13,11 @@ require('./auth');
 
 const app = express();
 
-app.use(cors());
+const corsOptions = {
+    origin: 'http://localhost:5173',
+    credentials: true,
+  };
+app.use(cors(corsOptions));
 app.use(express.json());
 
 connectToDatabase();
